a7fe482db1f1ea2b9b5521d50b63b7894438f380
Enzo Falvo <bestwordno@gmail.com>
2020-07-26 15:19:12 -0300

    Update department

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -17,6 +18,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.ToolBar;
@@ -31,12 +33,12 @@ import model.services.DepartmentService;
 public class DepartmentListController implements Initializable, DataChangeListener {
 
     private DepartmentService service;
-    
+
     @FXML
     private VBox vBox;
 
     @FXML
-    private TableView <Department> tableViewDepartment;
+    private TableView<Department> tableViewDepartment;
 
     @FXML
     private TableColumn<Department, Integer> tableColumnId;
@@ -45,13 +47,16 @@ public class DepartmentListController implements Initializable, DataChangeListen
     private TableColumn<Department, String> tableColumnName;
 
     @FXML
+    private TableColumn<Department, Department> tableColumnEDIT;
+
+    @FXML
     private Button btNew;
-    
+
     @FXML
     private ToolBar toolBar;
 
-    private ObservableList <Department> obsList;
-    
+    private ObservableList<Department> obsList;
+
     @FXML
     public void onBtNewAction(ActionEvent event) {
         Stage parentStage = Utils.currentStage(event);
@@ -70,8 +75,6 @@ public class DepartmentListController implements Initializable, DataChangeListen
     public void setTableColumnId(TableColumn<Department, Integer> tableColumnId) {
         this.tableColumnId = tableColumnId;
     }
-    
-    
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -87,28 +90,30 @@ public class DepartmentListController implements Initializable, DataChangeListen
         tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
 
     }
-    
+
     public void updateTableView() {
         if (service == null) {
             throw new IllegalStateException("Service was null");
         }
-        List <Department> list = service.findAll();
+        List<Department> list = service.findAll();
         obsList = FXCollections.observableArrayList(list);
         tableViewDepartment.setItems(obsList);
         
-        
+        initEditButtons();
+
     }
-    private void createDialogForm (Department obj, String absoluteName, Stage parentStage) {
+
+    private void createDialogForm(Department obj, String absoluteName, Stage parentStage) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
             Pane pane = loader.load();
-            
+
             DepartmentFormController controller = loader.getController();
             controller.setDepartment(obj);
             controller.setDepartmentService(new DepartmentService());
             controller.subscribeDataChangeListener(this);
             controller.updateFormData();
-            
+
             Stage dialogStage = new Stage();
             dialogStage.setTitle("Enter department data");
             dialogStage.setScene(new Scene(pane));
@@ -117,10 +122,8 @@ public class DepartmentListController implements Initializable, DataChangeListen
             // IF NOT CLOSE THE WINDOW, IS NOT CLOSE
             dialogStage.initModality(Modality.WINDOW_MODAL);
             dialogStage.showAndWait();
-            
-            
-        }
-        catch (IOException e) {
+
+        } catch (IOException e) {
             Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
         }
     }
@@ -128,7 +131,27 @@ public class DepartmentListController implements Initializable, DataChangeListen
     @Override
     public void onDataChanged() {
         updateTableView();
-        
+
+    }
+
+    private void initEditButtons() {
+        tableColumnEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+        tableColumnEDIT.setCellFactory(param -> new TableCell<Department, Department>() {
+            private final Button button = new Button("Edit");
+
+            @Override
+            protected void updateItem(Department obj, boolean empty) {
+                super.updateItem(obj, empty);
+                if (obj == null) {
+                    setGraphic(null);
+                    return;
+                }
+                setGraphic(button);
+                button.setOnAction(
+                        event -> createDialogForm(
+                                obj, "/gui/DepartmentForm.fxml", Utils.currentStage(event)));
+            }
+        });
     }
 
 }
