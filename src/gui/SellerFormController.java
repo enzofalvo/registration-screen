81dcf60d08a479bf8e883517ffa07e2a6e7036c7
Enzo Falvo <bestwordno@gmail.com>
2020-07-28 15:25:18 -0300

    TextField and DatePicker

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -6,8 +6,11 @@ import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
@@ -16,6 +19,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
@@ -28,20 +32,38 @@ public class SellerFormController implements Initializable {
     private Seller entity;
 
     private SellerService service;
-    
-    private List <DataChangeListener> dataChangeListener = new ArrayList<>();
+
+    private List<DataChangeListener> dataChangeListener = new ArrayList<>();
 
     @FXML
     private AnchorPane anchorPane;
-    
+
     @FXML
     private TextField txtId;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private DatePicker dpBirthDate;
+
+    @FXML
+    private TextField txtBaseSalary;
 
     @FXML
     private TextField txtName;
 
     @FXML
     private Label labelErrorName;
+
+    @FXML
+    private Label labelErrorEmail;
+
+    @FXML
+    private Label labelErrorBirthDate;
+
+    @FXML
+    private Label labelErrorBaseSalary;
 
     @FXML
     private Button btSave;
@@ -56,8 +78,8 @@ public class SellerFormController implements Initializable {
     public void setSellerService(SellerService service) {
         this.service = service;
     }
-    
-    public void subscribeDataChangeListener (DataChangeListener listener ) {
+
+    public void subscribeDataChangeListener(DataChangeListener listener) {
         dataChangeListener.add(listener);
     }
 
@@ -78,8 +100,7 @@ public class SellerFormController implements Initializable {
             Utils.currentStage(event).close();
         } catch (DbException e) {
             Alerts.showAlert("Error saving object", null, e.getMessage(), Alert.AlertType.ERROR);
-        }
-        catch (ValidationException e) {
+        } catch (ValidationException e) {
             setErrorMessages(e.getErrors());
         }
     }
@@ -98,7 +119,11 @@ public class SellerFormController implements Initializable {
 
     private void initializeNodes() {
         Constraints.setTextFieldInteger(txtId);
-        Constraints.setTextFieldMaxLength(txtName, 30);
+        Constraints.setTextFieldMaxLength(txtName, 70);
+        Constraints.setTextFieldDouble(txtBaseSalary);
+        Constraints.setTextFieldMaxLength(txtEmail, 60);
+        Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
+
     }
 
     public void updateFormData() {
@@ -108,22 +133,30 @@ public class SellerFormController implements Initializable {
 
         txtId.setText(String.valueOf(entity.getId()));
         txtName.setText(entity.getName());
+        txtEmail.setText(entity.getEmail());
+        Locale.setDefault(Locale.US);
+        txtBaseSalary.setText(String.format("%.2f", entity.getBaseSalary()));
+        if (entity.getBirthDate() != null) {
+
+            dpBirthDate.setValue(LocalDate.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()));
+        }
+
     }
 
     private Seller getFormData() {
-       
+
         Seller obj = new Seller();
-        
+
         ValidationException exception = new ValidationException("Error: Validation error");
-        
+
         obj.setId(Utils.tryParseToInt(txtId.getText()));
-        
+
         if (txtName.getText() == null || txtName.getText().trim().equals("")) {
             exception.addError("name", "Field can't be empty");
-            
+
         }
         obj.setName(txtName.getText());
-        
+
         if (exception.getErrors().size() > 0) {
             throw exception;
         }
@@ -136,14 +169,14 @@ public class SellerFormController implements Initializable {
             listener.onDataChanged();
         }
     }
-    
-    private void setErrorMessages(Map <String, String> errors) {
+
+    private void setErrorMessages(Map<String, String> errors) {
         Set<String> fields = errors.keySet();
-        
+
         if (fields.contains("name")) {
             labelErrorName.setText(errors.get("name"));
         }
-        
+
     }
 
 }
